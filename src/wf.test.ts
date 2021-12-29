9bd3e3a30c330736e9620ca0c60b98e9e58fd8a8
Drew De Ponte <cyphactor@gmail.com>
2021-12-29 14:08:29 -0500

    Add basics of web framework
    This includes the basics of request processing, routing, route handling,
    and the ability to test a routing configuration in isolation as well as
    being able to test the request processing end to end.

--- src/wf.test.ts
+++ src/wf.test.ts
@@ -1,4 +1,52 @@
+import * as wf from "./wf";
 
-test("make sure testing works", () => {
-	expect(1).toBe(1);
+test("end-to-end test request -> response", () => {
+  function somePathHandler(request: wf.Request): wf.Response {
+    return { status: 200 };
+  }
+
+  function notFoundHandler(request: wf.Request): wf.Response {
+    return { status: 404 };
+  }
+
+  const router = wf.router(
+    [{ pattern: "some/path", handler: somePathHandler }],
+    {
+      pattern: "",
+      handler: notFoundHandler,
+    }
+  );
+
+  const request: wf.Request = { path: "some/path" };
+
+  const response = wf.process(request, router);
+
+  expect(response.status).toBe(200);
+});
+
+test("test routing", () => {
+  function somePathHandler(request: wf.Request): wf.Response {
+    return { status: 200 };
+  }
+
+  function notFoundHandler(request: wf.Request): wf.Response {
+    return { status: 404 };
+  }
+
+  const router = wf.router(
+    [{ pattern: "some/path", handler: somePathHandler }],
+    {
+      pattern: "",
+      handler: notFoundHandler,
+    }
+  );
+
+  const request: wf.Request = { path: "some/path" };
+
+  const route = router(request);
+
+  expect(route).toStrictEqual({
+    pattern: "some/path",
+    handler: somePathHandler,
+  });
 });
---
+++ src/wf.ts
@@ -0,0 +1,39 @@
+export interface Request {
+  path: string;
+  // ...
+}
+
+export interface Response {
+  status: number;
+  // ...
+}
+
+export type RouteHandler = (request: Request) => Response;
+
+export interface Route {
+  pattern: string;
+  handler: RouteHandler;
+}
+
+export type Routes = Array<Route>;
+
+export type Router = (request: Request) => Route;
+
+function routeMatches(request: Request, route: Route): Bool {
+  return true;
+}
+
+export function router(routes: Routes, notFoundRoute: Route): Router {
+  return function (request: Request): Route {
+    const matchedRoute = routes.find((route) => routeMatches(request, route));
+    if (matchedRoute) {
+      return matchedRoute;
+    } else {
+      return notFoundRoute;
+    }
+  };
+}
+
+export function process(request: Request, router: Router): Response {
+  return router(request).handler(request);
+}
